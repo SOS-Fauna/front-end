184fa835e123f9c90855f040628e4f1681202bf6
DanielleFS <danielleferreirasilva@hotmail.com>
2025-03-21 23:22:37 -0300

    Ajustes de responsividade do Header e na tela home ajustes de escrita no código

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,61 @@
-import logo from "../assets/logo.svg"
-import iconperfil from "../assets/iconperfil.svg"
-import "../styles/header.css"
+import React, { useState, useEffect } from "react";
 
+import logo from "../assets/logo.svg";
+import iconperfil from "../assets/iconperfil.svg";
+import "../styles/header.css";
 
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [openMenuHamb, setOpenMenuHamb] = useState(false);
+  const resetMenu = () => {
+    setOpenMenuHamb(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resetMenu);
+    window.addEventListener("scroll", resetMenu);
+  }, []);
+
+  const clickMenu = () => {
+    setOpenMenuHamb(!openMenuHamb);
+  };
+
   return (
     <header id="header">
       <nav className="nav-bar">
-        <Link to="/">
+        <Link to="/" className="linkLogo" onClick={resetMenu}>
           <img src={logo} alt="Descrição da imagem" />
         </Link>
+        <div onClick={clickMenu}>
+          <div className="menu-hamb">
+            <div className={openMenuHamb ? "barra1-ative" : "barra1"}></div>
+            <div className={openMenuHamb ? "barra2-ative" : "barra2"}></div>
+            <div className={openMenuHamb ? "barra3-ative" : "barra3"}></div>
+          </div>
+          <ul className={openMenuHamb ? "nav-list on" : "nav-list"}>
+            <li className="nav-item">
+              <Link to="/Denuncie">Denuncie Aqui</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Ong">Encontre uma Ong</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/QuemSomos">Quem Somos</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Adocao">Adoção</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Veterinario">Atendimento Veterinário</Link>
+            </li>
+          </ul>
+        </div>
 
-        {/* <img src={logo} alt="" /> */}
-        <ul className="nav-list">
-          <li className="nav-item"><Link to="/" ></Link></li>
-          <li className="nav-item"><Link to="/Denuncie" >Denuncie Aqui</Link></li>
-          <li className="nav-item"><Link to="/Ong" >Encontre uma Ong</Link></li>
-          <li className="nav-item"><Link to="/QuemSomos" >Quem Somos</Link></li>
-          <li className="nav-item"><Link to="/Adocao" >Adoção</Link></li>
-          <li className="nav-item"><Link to="/Veterinario" >Atendimento Veterinário</Link></li>
-
-
-
-
-        </ul>
-        <img src={iconperfil} alt="" />
+        <div className="iconePerfil">
+          <img src={iconperfil} alt="" />
+        </div>
       </nav>
     </header>
-  )
-
+  );
 }
