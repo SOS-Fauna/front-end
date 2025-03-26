9588df2df47c9cb4c46081d9d0fe34163ca473af
DanielleFS <danielleferreirasilva@hotmail.com>
2025-03-26 01:15:12 -0300

    criação do footer base

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../styles/Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p><strong>Contato:</strong> sosfaunape@sosfauna.com</p>
+        </div>
+    )
+}
+export default Footer
