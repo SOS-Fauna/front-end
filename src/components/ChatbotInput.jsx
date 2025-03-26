885d7c8074da73188bbb15eedea7497db0d6d263
DanielleFS <danielleferreirasilva@hotmail.com>
2025-03-26 18:28:30 -0300

    inclusão de autofocus no chatbot

--- src/components/ChatbotInput.jsx
+++ src/components/ChatbotInput.jsx
@@ -12,6 +12,7 @@ function ChatbotInput({ type, placeholder, setValor, avancarEtapa, campoObrigato
                         onChange={(e) => setValor(e.target.value)}
                         required={campoObrigatorio}
                         minLength={tamanhoMin}
+                        autoFocus
                     />
 
                     <button className="action-button" type="submit"><LuSend size={20} color="white" /></button>
